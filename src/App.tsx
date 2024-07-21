32a4cb902595eab36c0da7c2227a4d75ad2a644a
adiel-madari <adielmad@gmail.com>
2024-07-21 13:03:01 +0300

    Added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { Space } from './themes/Space'
-// import { Socket } from './themes/Socket'
-// import { Elevate } from './themes/Elevate'
-// import { Future } from './themes/Future'
-// import { Empower } from './themes/Empower'
-// import { Vision } from './themes/Vision'
-
-function App() {
-  return <Space />
+export default function App() {
+  return (
+    <h1>Press and hold page navigation</h1>
+  )
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,60 @@
 import './styles/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import { Elevate } from './pages/elevate'
+import { Empower } from './pages/empower'
+import { Future } from './pages/future'
+import { Socket } from './pages/socket'
+import { Space } from './pages/space'
+import { Vision } from './pages/vision'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    loader: false,
+    element: <App />,
+  },
+  {
+    path: '/pages/',
+    children: [
+      {
+        path: 'elevate',
+        loader: false,
+        element: <Elevate />,
+      },
+      {
+        path: 'empower',
+        loader: false,
+        element: <Empower />,
+      },
+      {
+        path: 'future',
+        loader: false,
+        element: <Future />,
+      },
+      {
+        path: 'socket',
+        loader: false,
+        element: <Socket />,
+      },
+      {
+        path: 'space',
+        loader: false,
+        element: <Space />,
+      },
+      {
+        path: 'vision',
+        loader: false,
+        element: <Vision />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
