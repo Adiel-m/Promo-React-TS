c0bed2afb9dd4d42e6f7ea444aff569e841273c0
adiel-madari <adielmad@gmail.com>
2024-07-22 14:36:53 +0300

    Added NavMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { NavMenu } from "./components/NavMenu";
+
 export default function App() {
   return (
-    <h1>Press and hold on the page to show navigation menu</h1>
+    <>
+      <h1>Press and hold on the page to show navigation menu</h1>
+      <NavMenu />
+    </>
   )
 }
---
+++ src/components/NavMenu.tsx
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router-dom"
+import { navigation } from "./navigation"
+
+export const NavMenu = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        {navigation.map((item) => (
+          <li key={item.path}>
+            {!item.children ? (
+              <NavLink className="nav--link" to={item.path}>
+                {item.path === '/' ? 'home' : item.path}
+              </NavLink>
+            ) : (
+              <ul>
+                {item.children.map((subItem) => (
+                  <li key={subItem.path}>
+                    <NavLink
+                      className="nav--link"
+                      to={item.path + subItem.path}
+                    >
+                      {subItem.path}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
