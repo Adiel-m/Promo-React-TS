977ebd6f37db4678d2cdbbf2f6303a41209b6b2f
adiel-madari <adielmad@gmail.com>
2024-07-22 21:52:09 +0300

    NavMenu base structure and styles

--- src/components/navMenu/NavMenu.tsx
+++ src/components/navMenu/NavMenu.tsx
@@ -1,24 +1,26 @@
+import './nav.css'
 import { NavLink } from "react-router-dom"
 import { navigation } from "../navigation"
 
 export const NavMenu = () => {
   return (
     <nav className="nav">
-      <ul>
+      <ul className="menu">
         {navigation.map((item) => (
-          <li key={item.path}>
+          <li className="item" key={item.path}>
             {!item.children ? (
-              <NavLink className="nav--link" to={item.path}>
+              <NavLink className="link" to={item.path}>
                 {item.path === '/' ? 'home' : item.path}
               </NavLink>
             ) : (
-              <ul>
-                {item.children.map((subItem) => (
-                  <li key={subItem.path}>
-                    <NavLink
-                      className="nav--link"
-                      to={item.path + subItem.path}
-                    >
+              <ul className="sub-menu">
+                {item.children.map((subItem, i) => (
+                  <li
+                    style={{ offsetDistance: `${(100 / item.children.length) * (i+1)}%` }}
+                    className="sub-item"
+                    key={subItem.path}
+                  >
+                    <NavLink className="sub-link" to={item.path + subItem.path}>
                       {subItem.path}
                     </NavLink>
                   </li>
