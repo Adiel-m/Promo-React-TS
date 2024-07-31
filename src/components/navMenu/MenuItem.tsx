28f9ca3a230e28dc006b95461e0f1b22e76b178d
adiel-madari <adielmad@gmail.com>
2024-07-31 16:26:07 +0300

    Created MenuItem component

---
+++ src/components/navMenu/MenuItem.tsx
@@ -0,0 +1,45 @@
+import { NavLink, To } from "react-router-dom"
+import { useContext } from "react"
+import { MenuContext } from "./MenuContext"
+
+interface MenuItemProps {
+  itemRef: React.LegacyRef<HTMLLIElement> | undefined
+  itemClass: string
+  style: React.CSSProperties | undefined
+  linkLabel: string
+  linkClass: string
+  linkTo: To
+  children?: JSX.Element
+}
+
+
+export const MenuItem = ({
+  itemRef,
+  itemClass,
+  style,
+  linkLabel,
+  linkClass,
+  linkTo,
+  children,
+}: MenuItemProps) => {
+  const { handleHoverLeave, handleHoverOver } = useContext(MenuContext)
+
+  return (
+    <li
+      ref={itemRef}
+      onMouseOver={(e) => {
+        handleHoverOver(e)
+      }}
+      onMouseLeave={(e) => {
+        handleHoverLeave(e)
+      }}
+      className={itemClass}
+      style={style}
+    >
+      <NavLink className={linkClass} to={linkTo}>
+        {linkLabel}
+      </NavLink>
+      {children}
+    </li>
+  )
+}
