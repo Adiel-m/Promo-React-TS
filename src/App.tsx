5c9e263802667a8a732f31adc2abe6b2051a4791
adiel-madari <adielmad@gmail.com>
2024-08-01 10:00:14 +0300

    Wraped the inner content in Theme component

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,20 @@ export default function App() {
   const { handleMouseDown, handleMouseUp, handleTouchStart, handleTouchEnd } = useContext(MenuContext)
 
   return (
-    <div
-      style={{ minHeight: '100dvh' }}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-      onTouchStart={handleTouchStart}
-      onTouchEnd={handleTouchEnd}
-    >
     <Theme themePage="home" theme="light">
-      <Header>
-        <h1>Press and Hold for 1 Second To View the Menu</h1>
-        <Nav />
-      </Header>
-      <Outlet />
+      <div
+        style={{ minHeight: '100dvh' }}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+      >
+        <Header>
+          <h1>Press and Hold for 1 Second To View the Menu</h1>
+          <Nav />
+        </Header>
+        <Outlet />
+      </div>
     </Theme>
-    </div>
   )
 }
